28d507163998eb66d120bd17a43d36b71487a1ca
oblivion4us <nft4oblivion@gmail.com>
2024-08-22 23:13:39 +0200

    preparing for encapsulation

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 class User():
-    def __init__(self, ID, name, access_level="user"):
-        self.__private_ID = f"{ID:04}"
-        self.__private_name = name
-        self.__private_access_level = access_level
+    def __init__(self, ID, name):
+        self.ID = f"{ID:04}"
+        self.name = name
+        self.access_level = "user"
 
 class Admin(User):
-    def __init__(self, ID, name, access_level="admin"):
-        super().__init__(ID, name)
-        self.access_level = access_level
+    def __init__(self, ID, name):
+        super().__init__(f"{int(ID):04}", name)
+        self.access_level = "admin"
         self.users_list = {}
 
     def add_user(self, ID, name):
-        self.users_list[ID] = name
+        user_id = f"{int(ID):04}"
+        if user_id in self.users_list:
+            print(f"ID {user_id} уже занят")
+        else:
+            self.users_list[user_id] = name
+            print(f"Пользователь {name} с ID {user_id} добавлен")
 
     def delete_user(self, ID):
-        if ID in self.users_list:
-            del self.users_list[ID]
+        user_id = f"{int(ID):04}"
+        if user_id in self.users_list:
+            del self.users_list[user_id]
+            print(f"Пользователь {int(ID):04} удален")
         else:
-            print(f"Пользователь {ID} не найден")
+            print(f"Пользователь {user_id} не найден")
 
     def display_users(self):
-        print(f"Список пользователей:\n {self.users_list}")
+        print("Список пользователей:")
+        for user_id, name in self.users_list.items():
+            print(f"{user_id}: {name}")
 
-users_1b = Admin(1, "Админ_Валерий")
-users_1b.add_user(10, "Пользователь_Игорь")
-users_1b.add_user(11, "Пользователь_Игорь")
+users_1b = Admin(1, "Валерий")
+users_1b.add_user(1101, "Игорь")
+users_1b.add_user(99, "Елисей")
+users_1b.add_user(20, "Константин")
+users_1b.add_user(20, "Виктор")
 users_1b.display_users()
-
