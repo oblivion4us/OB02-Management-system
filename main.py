ad81bfb1759ea8d9225feed9194f608c293f5062
oblivion4us <nft4oblivion@gmail.com>
2024-08-22 23:45:38 +0200

    add encapsulation for ID, name and access_level. Adding set and get constructs

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-class User():
+class User:
     def __init__(self, ID, name):
-        self.ID = f"{ID:04}"
-        self.name = name
-        self.access_level = "user"
+        self.__ID = f"{int(ID):04}"
+        self.__name = name
+        self.__access_level = "user"
+
+    def get_user_ID(self):
+        return self.__ID
+
+    def get_user_name(self):
+        return self.__name
+
+    def get_access_level(self):
+        return self.__access_level
+
+    def set_user_name(self, name):
+        self.__name = name
+
 
 class Admin(User):
     def __init__(self, ID, name):
-        super().__init__(f"{int(ID):04}", name)
+        super().__init__(ID, name)
         self.access_level = "admin"
         self.users_list = {}
 
-    def add_user(self, ID, name):
-        user_id = f"{int(ID):04}"
+    def add_user(self, user):
+        user_id = user.get_user_ID()
+        user_name = user.get_user_name()
         if user_id in self.users_list:
             print(f"ID {user_id} уже занят")
         else:
-            self.users_list[user_id] = name
-            print(f"Пользователь {name} с ID {user_id} добавлен")
+            self.users_list[user_id] = user_name
+            print(f"Пользователь {user_name} с ID {user_id} добавлен")
 
-    def delete_user(self, ID):
+    def remove_user(self, ID):
         user_id = f"{int(ID):04}"
         if user_id in self.users_list:
             del self.users_list[user_id]
-            print(f"Пользователь {int(ID):04} удален")
+            print(f"Пользователь {user_id} удален")
         else:
             print(f"Пользователь {user_id} не найден")
 
@@ -31,9 +45,16 @@ class Admin(User):
         for user_id, name in self.users_list.items():
             print(f"{user_id}: {name}")
 
-users_1b = Admin(1, "Валерий")
-users_1b.add_user(1101, "Игорь")
-users_1b.add_user(99, "Елисей")
-users_1b.add_user(20, "Константин")
-users_1b.add_user(20, "Виктор")
-users_1b.display_users()
+admin = Admin(1, "Константин")
+user1 = User(11, "Андрей")
+user2 = User(12, "Валерий")
+admin.add_user(user1)
+admin.add_user(user2)
+user3 = User(12, "Анатолий")
+user4 = User(101, "Георгий")
+admin.add_user(user3)
+admin.add_user(user4)
+admin.display_users()
+admin.remove_user(101)
+admin.display_users()
+
